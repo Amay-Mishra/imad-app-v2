a208361cf7aefda400077257e62a3810aa7d80e6
Amay-Mishra <amaymishra11@gmail.com>
2017-02-26 16:18:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/DSC02540.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'DSC02540.JPG'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
