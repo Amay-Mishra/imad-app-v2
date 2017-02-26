e97615e9dfb096ebcda9a9e6a469ac97471f8612
Amay-Mishra <amaymishra11@gmail.com>
2017-02-26 15:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/mDSC02540.JPG', function (req, res) {
+app.get('/ui/DSC02540.JPG', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'DSC02540.JPG'));
 });
 
