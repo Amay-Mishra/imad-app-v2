f10fb3cfb558bd939355b7587f94a3e15adcd6c0
Amay-Mishra <amaymishra11@gmail.com>
2017-03-08 12:47:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,30 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+  //host: 'localhost',
+  user: 'amay-mishra',
+  database: 'amay-mishra',
+  host: 'db.imad.hasura-app.io',
+  //path: '/database.php?pgsql=localhost%3A5432',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
+
 app.use(expres.static("public"));
 app.use('/ui',express.static(__dirname+'/ui'));
+
+app.get('/test-db', function(req,res) {
+  //make a select request
+    pool.query('SELECT * FROM articles',function(err,result){
+       if (err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result.rows));
+    }
+    });
+  //return a response
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
