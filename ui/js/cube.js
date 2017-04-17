8e24db9dd2416bbafe0b45e9791dea8e481ff470
Amay-Mishra <amaymishra11@gmail.com>
2017-04-17 17:42:09 +0530

    [imad-console] Updates ui/js/cube.js

--- ui/js/cube.js
+++ ui/js/cube.js
@@ -12,11 +12,13 @@ var init = function() {
   for (var i=0, len = showPanelButtons.length; i < len; i++) {
     showPanelButtons[i].addEventListener( 'click', onButtonClick, false);
   }
+  toggle();
     console.log(box);
-//   document.getElementById('toggle-backface-visibility').addEventListener( 'click', function(){
-//      box.toggleClassName('panels-backface-invisible');
-//   }, false);
 
 };
+
+var toggle = function(){
+     box.toggleClassName('panels-backface-invisible');
+  };
 
 window.addEventListener( 'DOMContentLoaded', init, false);
